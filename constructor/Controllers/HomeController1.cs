f99d9a5e9f17bb371589d738c879fa2394ac8703
malikckkpln <malikucukkaplan@gmail.com>
2023-02-24 01:14:20 +0300

    Add project files.

---
+++ constructor/Controllers/HomeController1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace constructor.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public IActionResult Index(int sayi1=0, int sayi2 = 0)
+        {
+            int sonuc = sayi1 + sayi2;
+            ViewBag.snc = sonuc;
+            return View();
+        }
+    }
+}
